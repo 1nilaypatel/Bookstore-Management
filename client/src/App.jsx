93fffedc4adfae2f1fd7dab9e2aa729358bca356
1nilaypatel <nilaynilaypatelpatel@gmail.com>
2024-02-12 20:12:15 +0530

    _Create_pages and components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import React from 'react';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Appbar from './components/Appbar.jsx';
+import BookList from './pages/BookList.jsx';
+import Home from './pages/Home.jsx';
 
 export default function App() {
   return (
-    <div className='text-red-500'>App</div>
+    <div>
+      <Router>
+        <Appbar/>
+        <Routes>
+          <Route path={"/"} element={<Home />} />
+          <Route path={"/book-list"} element={<BookList />} />
+        </Routes>
+      </Router>
+    </div>
   )
 }
